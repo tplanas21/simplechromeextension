b4173d77147c4d2f40ba17416049aa4b06762c2c
theaplanas21 <kanekiyeols@gmail.com>
2023-09-12 01:25:45 -0400

    change image pt2

--- js/newtab.js
+++ js/newtab.js
@@ -1,13 +1,40 @@
-function fetchImg(){
-    fetch("https://source.unsplash.com/random/1920x1080/?modern")
+// function fetchImg(){
+//     fetch('https://source.unsplash.com/random/1920x1080/?modern')
+//         .then(response => response.blob())
+//         .then(blob => {
+//             const imgUrl = URL.createObjectURL(blob);
+//             const imgElem = document.createElement("img");
+
+//             document.getElementById("img").src = imgUrl;
+           
+//             document.body.appendChild(imgElem);
+//         })
+//         .catch(error => {
+//             console.log("Error")
+//         });
+
+// }
+
+function get_random_dog_image(){
+
+    url = "https://dog.ceo/api/breeds/image/random";
+  
+    fetch(url)
     .then(function(response){
-        return response.json();
+      return response.json();
     })
-    .then(data => {
-        const imgUrl = URL.createObjectURL(data);
-        document.getElementById("img").src = imgUrl;
+    .then(function(data){
+      display_image(data.message);
     })
-    .catch(error => {
-        console.log("Error")
+    .catch(function(error){
+      console.log("Error: " + error);
     });
-}
+  
+  
+  }
+  
+  function display_image(image_url){
+    document.getElementById("image").src = image_url;
+  }
+  
+  
