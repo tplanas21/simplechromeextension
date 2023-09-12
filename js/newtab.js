378fdc052b1f2fca32fe0f84c9fc38165a04e48b
theaplanas21 <kanekiyeols@gmail.com>
2023-09-12 02:21:51 -0400

    change background 3

--- js/newtab.js
+++ js/newtab.js
@@ -1,40 +1,13 @@
-// function fetchImg(){
-//     fetch('https://source.unsplash.com/random/1920x1080/?modern')
-//         .then(response => response.blob())
-//         .then(blob => {
-//             const imgUrl = URL.createObjectURL(blob);
-//             const imgElem = document.createElement("img");
-
-//             document.getElementById("img").src = imgUrl;
-           
-//             document.body.appendChild(imgElem);
-//         })
-//         .catch(error => {
-//             console.log("Error")
-//         });
-
-// }
-
-function get_random_dog_image(){
-
-    url = "https://dog.ceo/api/breeds/image/random";
-  
+function setBackgroundImg(){
+    let url = "https://source.unsplash.com/random/1920x1080/?modern"
     fetch(url)
-    .then(function(response){
-      return response.json();
-    })
-    .then(function(data){
-      display_image(data.message);
-    })
-    .catch(function(error){
-      console.log("Error: " + error);
-    });
-  
-  
-  }
-  
-  function display_image(image_url){
-    document.getElementById("image").src = image_url;
-  }
-  
-  
+        .then(response=>response.json())
+        .then(data =>{
+           // document.getElementById("image").src = data.message;
+           document.body.style.backgroundImage = url;
+        })
+        .catch(error => {
+            console.log("error");
+        });
+}
+
